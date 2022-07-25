853d9dddc6463b10405c92c16dab15321a9e63a9
Galaxy-sz <1355594513@qq.com>
2022-07-26 01:16:10 +0800

    add Q56

---
+++ 56/_56.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+
+    }
+};
+
+using namespace std;
+
+int main(){
+
+    return 0;
+}
