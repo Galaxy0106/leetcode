301f61070ab0a8a893bd5a9654c2c20bd98477fd
Galaxy-sz <1355594513@qq.com>
2022-08-31 00:44:58 +0800

    add Q347

--- 347/347.cpp
+++ 347/347.cpp
@@ -4,12 +4,48 @@ using namespace std;
 
 class Solution {
 public:
+    struct cmp{
+        bool operator() (pair<int,int> a, pair<int,int> b) {
+            return a.second > b.second;
+        }
+    };
+
     vector<int> topKFrequent(vector<int>& nums, int k) {
-        
+        unordered_map<int, int> mp;    
+        priority_queue<pair<int,int>, vector< pair<int,int> >, cmp> pq;
+        vector<int> res;
+        // 遍历记录次数
+        for(int i = 0;i < nums.size();i++){
+            mp[nums[i]]++;
+        }
+        // 维护一个小根堆
+        for(auto iter = mp.begin();iter != mp.end();iter++){
+            if(pq.size() < k){
+                pq.push({iter->first, iter->second});
+            }
+            else{
+                if(iter->second > pq.top().second){
+                    pq.pop();
+                    pq.push({iter->first, iter->second});
+                }
+            }
+        }
+        while(!pq.empty()){
+            res.push_back(pq.top().first);
+            pq.pop();
+        }
+        return res;
     }
 };
 
 int main(){
-
+    Solution sol;
+    vector<int> nums = {1,1,1,2,2,3};
+    vector<int> res;
+    int k = 2;
+    res = sol.topKFrequent(nums, k);
+    for(int i = 0;i < res.size();i++){
+        cout << res[i] << " ";
+    }
     return 0;
 }
