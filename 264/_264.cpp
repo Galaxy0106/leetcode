2ce6f0a660993bd6235341e8b25f26a3a9fdf4f6
Galaxy-sz <1355594513@qq.com>
2022-09-12 20:40:16 +0800

    add Q264

--- 264/_264.cpp
+++ 264/_264.cpp
@@ -4,31 +4,37 @@ using namespace std;
 
 class Solution {
 public:
-    void initVec(vector<int>& vec, int k, int n){
-        vec.push_back(1);
-        int cur = 1;
-        while(cur*k <= n){
-            vec.push_back(cur*k);
-            cur = cur*k;
-        }
-        return;
+    int min3(int a, int b, int c){
+        return min(a, min(b, c));
     }
 
     int nthUglyNumber(int n) {
-        vector<int> vec2, vec3, vec5;
-        initVec(vec2, 2, n);
-        initVec(vec3, 3, n);
-        initVec(vec5, 5, n);
-        int i = 0, j = 0, k = 0;
-
-
-
-
+        vector<int> nums(n);
+        int p = 0;
+        int num2 = 1, num3 = 1, num5 = 1;
+        int p1 = 0, p2 = 0, p3 = 0;
+        while(p < n){
+            int temp = min3(num2, num3, num5);
+            nums[p++] = temp;
+            if(temp == num2){
+                num2 = 2 * nums[p1];
+                p1++;
+            }
+            if(temp == num3){
+                num3 = 3 * nums[p2];
+                p2++;
+            }
+            if(temp == num5){
+                num5 = 5 * nums[p3];
+                p3++;
+            }
+        }
+        return nums[n-1];
     }
 };
 
 int main(){
-
-
+    Solution sol;
+    cout << sol.nthUglyNumber(10);
     return 0;
 }
