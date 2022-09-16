cf0f9f9b2ed606948167fd2ce655e986554d7cb4
Galaxy-sz <1355594513@qq.com>
2022-09-16 18:50:06 +0800

    update Q394

--- 394/_394.cpp
+++ 394/_394.cpp
@@ -1,31 +1,59 @@
-#include <iostream>
 #include <bits/stdc++.h>
 
 using namespace std;
 
 class Solution {
 public:
-    string res;
-    void helper(string s, int index){
-        if(index == s.size()){
-            return;
-        }
-        // if(s[index] == )
-
-        // if(s[index] == '['){
-
-        // }
-
-    }
-
     string decodeString(string s) {
-        int index = 0;
-        helper(res, index);
+        // 定义数字栈和符号栈
+        stack<int> stk1;
+        stack<char> stk2;
+        int num;
+        string ntemp;
+        for(int i = 0;i < s.size();i++){
+            if(s[i] >= '0' && s[i] <= '9'){
+                ntemp.push_back(s[i]);
+            }
+            else if(s[i] == '['){
+                num = stoi(ntemp);
+                stk1.push(num);
+                ntemp.clear();
+                stk2.push('[');
+            }
+            else if(s[i] >= 'a' && s[i] <= 'z'){
+                stk2.push(s[i]);
+            }
+            else{
+                int top = stk1.top();
+                stk1.pop();
+                string temp;
+                while(!stk2.empty()){
+                    char c = stk2.top();
+                    stk2.pop();
+                    if(c == '[')
+                        break;
+                    temp.push_back(c);
+                }
+                while(top--){
+                    for(int j = temp.size()-1;j >= 0;j--){
+                        stk2.push(temp[j]);
+                    }
+                }
+            }
+        }
+        string res;
+        while(!stk2.empty()){
+            char c = stk2.top();
+            res.push_back(c);
+            stk2.pop();
+        }
+        reverse(res.begin(), res.end());
         return res;
     }
 };
 
 int main(){
-
+    Solution sol;
+    cout << sol.decodeString("abc3[cd]xyz") << endl;
     return 0;
 }
